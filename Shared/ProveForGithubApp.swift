d5f6056c2747e7e70afb00d0f2fd411a526b3be9
Blundgren62 <blundgren62@icloud.com>
2020-12-11 06:45:43 +0100

    Initial Commit

---
+++ Shared/ProveForGithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProveForGithubApp.swift
+//  Shared
+//
+//  Created by Bent Lundgren on 11/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct ProveForGithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
